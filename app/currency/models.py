ec110aff8c3937f3eee8ae321687eec241cd873b
Serhii <kirpa.sergej@gmail.com>
2021-06-09 23:11:29 +0300

    Create model Rate

--- app/currency/models.py
+++ app/currency/models.py
@@ -1,3 +1,8 @@
-# from django.db import models
+from django.db import models
 
-# Create your models here.
+
+class Rate(models.Model):
+    type = models.CharField(max_length=5)
+    sale = models.DecimalField(max_digits=5, decimal_places=2)
+    buy = models.DecimalField(max_digits=5, decimal_places=2)
+    created = models.DateTimeField(auto_now_add=True)
