65cd6cca27a07feee3cdde6ac5436283e566259a
Serhii <kirpa.sergej@gmail.com>
2021-06-09 23:46:42 +0300

    Write class ContactUs

--- app/currency/models.py
+++ app/currency/models.py
@@ -3,6 +3,15 @@ from django.db import models
 
 class Rate(models.Model):
     type = models.CharField(max_length=5)
+    source = models.CharField(max_length=64)
     sale = models.DecimalField(max_digits=5, decimal_places=2)
     buy = models.DecimalField(max_digits=5, decimal_places=2)
+    created = models.DateTimeField(auto_now_add=True)
+
+
+class ContactUS(models.Model):
+    id = models.CharField(max_length=5)
+    email_from = models.CharField(max_length=64)
+    subject = models.DecimalField(max_digits=5, decimal_places=2)
+    message = models.DecimalField(max_digits=5, decimal_places=2)
     created = models.DateTimeField(auto_now_add=True)
