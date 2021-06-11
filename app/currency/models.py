ffdd547259653afc5b25806ca9715ad6f9d2e111
Serhii <kirpa.sergej@gmail.com>
2021-06-11 09:29:49 +0300

    class Rate 'type' changed on 'currency_name'

--- app/currency/models.py
+++ app/currency/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Rate(models.Model):
-    type = models.CharField(max_length=5)
+    currency_name = models.CharField(max_length=5)
     source = models.CharField(max_length=64)
     sale = models.DecimalField(max_digits=5, decimal_places=2)
     buy = models.DecimalField(max_digits=5, decimal_places=2)
