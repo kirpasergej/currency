caee555bb4ce535ed078e474cf87c6526fa4aaeb
Serhii <kirpa.sergej@gmail.com>
2021-06-14 23:40:05 +0300

    model Source created

--- app/currency/views.py
+++ app/currency/views.py
@@ -1,9 +1,9 @@
-# from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.shortcuts import render
 from currency.utils import generate_password as gp
 
-from currency.utils import hello_world as hw
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+from currency.models import Rate
 
 
 def generate_password(request):
@@ -11,6 +11,24 @@ def generate_password(request):
     return HttpResponse(password)
 
 
-def hello_world(request):
-    hello = hw()
-    return HttpResponse(hello)
+def rate_list(request):
+    queryset = Rate.objects.all()
+
+    cont = {
+
+        'objects': queryset,
+    }
+
+    return render(request, 'rate_list.html', context=cont)
+
+
+def rate_details(request, pk):
+    #    try:
+    #       rate = Rate.objects.get(pk=pk)
+    #    except Rate.DoesNotExist:
+    #        raise Http404(f"Rate does not exist with id{pk}")
+    rate = get_object_or_404(Rate, pk=pk)
+    cont = {
+        'object': rate,
+    }
+    return render(request, 'rate_details.html', context=cont)
